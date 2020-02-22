878690baa6a6a3d628f306e24a4a3f1134a8bd7c
Mbm7280 <56158949+Mbm7280@users.noreply.github.com>
2020-02-22 18:16:55 +0800

    Create CodeStatue.java

---
+++ src/main/java/com/home/mbm/status/CodeStatue.java
@@ -0,0 +1,28 @@
+package com.home.mbm.status;
+
+/**
+* @Package: com.home.mbm.status
+* @ClassName: CodeStatue
+* @Description: status
+* @Author: mbm
+* @date: 2020/2/4 20:04
+* @Version: 1.0
+*/
+public class CodeStatue {
+
+    public static final String CODE = "code";
+    public static final String DATA = "data";
+    public static final String MSG = "msg";
+
+    public static String getCODE() {
+        return CODE;
+    }
+
+    public static String getDATA() {
+        return DATA;
+    }
+
+    public static String getMSG() {
+        return MSG;
+    }
+}
